b28e321412df799b836483fd96d697f4cdabe933
Sean Wachs <wachs@live.dk>
2021-02-01 12:31:21 +0100

    updated print statement and force port 5000

--- itu-minitwit/minitwit.py
+++ itu-minitwit/minitwit.py
@@ -94,7 +94,7 @@ def timeline():
     redirect to the public timeline.  This timeline shows the user's
     messages as well as all the messages of followed users.
     """
-    print "We got a visitor from: " + str(request.remote_addr)
+    print ("We got a visitor from: " + str(request.remote_addr))
     if not g.user:
         return redirect(url_for('public_timeline'))
     offset = request.args.get('offset', type=int)
@@ -248,4 +248,4 @@ app.debug = DEBUG
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
