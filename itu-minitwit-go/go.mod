1fc37ddeb40e1773817b9eaabd7e018765431fdb
rdmo <rdmo@itu.dk>
2021-02-18 13:52:29 +0100

    -Introduced GORM tool for retrieving and reading the database.  -- No migrations has been setup yet, but the functionality works.
    -Modified the schema to be plural form for it to work with GORM.
    
    -Added models.go file, that inclues the types related to the database
     -- To run both files: "go run minitwit.go models.go"

--- itu-minitwit-go/go.mod
+++ itu-minitwit-go/go.mod
@@ -1,10 +1,12 @@
 module github.com/JonasWG/ITU-DevOps
 
-go 1.13
+go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/sessions v1.2.1
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.20.12
 )
--- itu-minitwit-go/minitwit.go
+++ itu-minitwit-go/minitwit.go
@@ -15,6 +15,8 @@ import (
 	"github.com/gorilla/sessions"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 const DRIVER = "sqlite3"
@@ -23,43 +25,30 @@ const PER_PAGE = 30
 const DEBUG = true
 const SECRET_KEY = "development key"
 
-type User struct {
-	User_id  int
-	Username string
-	Email    string
-	Pw_hash  string
-}
-
-type Message struct {
-	Message_id int
-	Author_id  int
-	Text       string
-	Pub_date   string
-	Flagged    int
-}
-
-func GetUserByUsername(username string, db *sql.DB) (User, error) {
+func GetUserByUsername(username string, db *gorm.DB) (User, error) {
 	user := User{}
-	err := db.QueryRow("SELECT * FROM user WHERE username= ?", username, 1).
-		Scan(&user.User_id, &user.Username, &user.Email, &user.Pw_hash)
+	result := db.Where("username = ?", username).First(&user)
+	// err := db.QueryRow("SELECT * FROM user WHERE username= ?", username, 1).
+	// 	Scan(&user.UserID, &user.Username, &user.Email, &user.PwHash)
 
-	return user, err
+	return user, result.Error
 }
 
-func GetUserById(id int, db *sql.DB) (User, error) {
+func GetUserById(id uint, db *gorm.DB) (User, error) {
 	user := User{}
-	err := db.QueryRow("SELECT * FROM user WHERE user_id= ?", id, 1).
-		Scan(&user.User_id, &user.Username, &user.Email, &user.Pw_hash)
+	result := db.First(&user, id)
+	// err := db.QueryRow("SELECT * FROM user WHERE user_id= ?", id, 1).
+	// 	Scan(&user.UserID, &user.Username, &user.Email, &user.PwHash)
 
-	return user, err
+	return user, result.Error
 }
 
-func LoginHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
+func LoginHandler(store *sessions.CookieStore, db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "session_cookie")
 
 		userId := session.Values["user_id"]
-		if isLoggedIn := userId != nil; isLoggedIn {
+		if isLoggedIn := userId != ""; isLoggedIn {
 			http.Redirect(w, r, "/", http.StatusFound)
 		}
 
@@ -68,11 +57,12 @@ func LoginHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 			user, err := GetUserByUsername(r.FormValue("username"), db)
 			if err != nil {
 				errorMsg = "Invalid username"
-			} else if err = bcrypt.CompareHashAndPassword([]byte(user.Pw_hash), []byte(r.FormValue("password"))); err != nil {
+				log.Println(err)
+			} else if err = bcrypt.CompareHashAndPassword([]byte(user.PwHash), []byte(r.FormValue("password"))); err != nil {
 				errorMsg = "Invalid password"
 			} else {
 				session.AddFlash("You were logged in")
-				session.Values["user_id"] = user.User_id
+				session.Values["user_id"] = user.UserID
 				err = session.Save(r, w)
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -88,7 +78,7 @@ func LoginHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 	})
 }
 
-func LogoutHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
+func LogoutHandler(store *sessions.CookieStore, db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "session_cookie")
 		session.Values["user_id"] = nil
@@ -104,7 +94,7 @@ func LogoutHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 	})
 }
 
-func RegisterHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
+func RegisterHandler(store *sessions.CookieStore, db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		session, _ := store.Get(r, "session_cookie")
 		isLoggedIn := session.Values["user_id"] != nil
@@ -125,21 +115,30 @@ func RegisterHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 			} else if user, _ := GetUserByUsername(r.FormValue("username"), db); user.Username == r.FormValue("username") {
 				errorMsg = "This username is already taken"
 			} else {
-				statement, err := db.Prepare("INSERT INTO user (username, email, pw_hash) values (?,?,?)")
-				if err != nil {
-					log.Println(err)
-					return
-				}
-				defer statement.Close()
+				// statement, err := sql.Prepare("INSERT INTO user (username, email, pw_hash) values (?,?,?)")
+				// if err != nil {
+				// 	log.Println(err)
+				// 	return
+				// }
+				// defer statement.Close()
 
 				pass := r.FormValue("password")
 				hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.MinCost)
+
 				if err != nil {
 					log.Println(err)
 					return
 				}
 
-				statement.Exec(r.FormValue("username"), r.FormValue("email"), hash)
+				// statement.Exec(r.FormValue("username"), r.FormValue("email"), hash)
+				user := User{
+					Username: r.FormValue("username"),
+					Email:    r.FormValue("email"),
+					PwHash:   string(hash),
+				}
+
+				db.Create(&user)
+				http.Redirect(w, r, "/login", http.StatusCreated)
 				// TODO return successful registration status
 			}
 		}
@@ -151,7 +150,7 @@ func RegisterHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 	})
 }
 
-func GetUserByIdHandler(db *sql.DB) http.Handler {
+func GetUserByIdHandler(db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		params := mux.Vars(r)
 		id, err := strconv.Atoi(params["id"])
@@ -160,7 +159,7 @@ func GetUserByIdHandler(db *sql.DB) http.Handler {
 			return
 		}
 
-		user, err := GetUserById(id, db)
+		user, err := GetUserById((uint(id)), db)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			return
@@ -200,7 +199,7 @@ func HomeHandler() http.Handler {
 	})
 }
 
-func AddMessageHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
+func AddMessageHandler(store *sessions.CookieStore, db *gorm.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		session, _ := store.Get(r, "session_cookie")
@@ -211,10 +210,13 @@ func AddMessageHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 		}
 		textValue := r.FormValue("text")
 		if textValue != "" {
-			result, error := db.Exec("INSERT INTO message (author_id, text, pub_date, flagged) VALUES (?, ?, ?, 0)", userId.(int), textValue, time.Now().Unix())
+			message := Message{Author_id: userId.(uint), Text: textValue, Pub_date: strconv.Itoa(int(time.Now().Unix())), Flagged: 0}
+			result := db.Create(&message)
+			// result := db.Exec("INSERT INTO message (author_id, text, pub_date, flagged) VALUES (?, ?, ?, 0)", userId.(int), textValue, time.Now().Unix())
 			log.Println(result)
-			if error != nil {
-				log.Fatal(error)
+
+			if result.Error != nil {
+				log.Fatal(result.Error)
 			}
 			session.AddFlash("Your message was recorded")
 			http.Redirect(w, r, "/", http.StatusFound)
@@ -222,40 +224,19 @@ func AddMessageHandler(store *sessions.CookieStore, db *sql.DB) http.Handler {
 	})
 }
 
-func GetMessageByString(store *sessions.CookieStore, db *sql.DB) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		// session, _ := store.Get(r, "session_cookie")
-		// userId := session.Values["user_id"]
-
-		messageQuery := r.FormValue("text")
-		if messageQuery != "" {
-			message := Message{}
-			rows := db.QueryRow("SELECT * FROM message WHERE message.text = ?", messageQuery, 1).
-				Scan(&message.Message_id, &message.Author_id, &message.Text, &message.Pub_date, &message.Flagged)
-			log.Println(rows)
-			log.Println(message)
-			// if err != nil {
-			// 	log.Fatal(err)
-			// }
-			http.Redirect(w, r, "/", http.StatusFound)
-		}
-	})
-}
-
-func BeforeRequestMiddleware(store *sessions.CookieStore, db *sql.DB) func(http.Handler) http.Handler {
+func BeforeRequestMiddleware(store *sessions.CookieStore, db *gorm.DB) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		mdfn := func(w http.ResponseWriter, r *http.Request) {
 			session, _ := store.Get(r, "session_cookie")
 			userId := session.Values["user_id"]
 			if userId != nil {
-				id := userId.(int)
+				id := userId.(uint)
 				user, err := GetUserById(id, db)
 				if err != nil {
 					log.Print(err)
 				}
 
-				session.Values["user_id"] = user.User_id
+				session.Values["user_id"] = user.UserID
 				err = session.Save(r, w)
 				if err != nil {
 					http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -270,38 +251,41 @@ func BeforeRequestMiddleware(store *sessions.CookieStore, db *sql.DB) func(http.
 	}
 }
 
-func initDb(driver string, datasource string) (*sql.DB, error) {
-	db, err := sql.Open(driver, datasource)
+func initDb(driver string, datasource string) (*gorm.DB, error) {
+	db, err := gorm.Open(sqlite.Open(datasource), &gorm.Config{})
+	sql, _ := db.DB()
+
+	// db, err := sql.Open(driver, datasource)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return db, db.Ping()
+	return db, sql.Ping()
 }
 
 func main() {
-	db, err := initDb(DRIVER, DATABASE)
+	gorm, err := initDb(DRIVER, DATABASE)
+	// sql, _ := gorm.DB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	// defer db.Close()
 
 	store := sessions.NewCookieStore([]byte(SECRET_KEY))
 
 	r := mux.NewRouter()
-	r.Use(BeforeRequestMiddleware(store, db))
+	r.Use(BeforeRequestMiddleware(store, gorm))
 	r.Handle("/", HomeHandler()).Methods("GET")
-	r.Handle("/public", TestHandler(db)).Methods("GET")
-	r.Handle("/login", LoginHandler(store, db)).Methods("GET", "POST")
-	r.Handle("/register", RegisterHandler(store, db)).Methods("GET", "POST")
-	r.Handle("/logout", LogoutHandler(store, db)).Methods("GET")
-	r.Handle("/add_message", TestHandler(db)).Methods("POST")
-	r.Handle("/{username}", TestHandler(db)).Methods("GET")
-	r.Handle("/{username}/follow", TestHandler(db)).Methods("GET")
-	r.Handle("/{username}/unfollow", TestHandler(db)).Methods("GET")
-	r.Handle("/test", TestHandler(db)).Methods("GET")
-	r.Handle("/user/{id}", GetUserByIdHandler(db)).Methods("GET")
-	r.Handle("/get_message", GetMessageByString(store, db)).Methods("GET")
+	// r.Handle("/public", TestHandler(sql)).Methods("GET")
+	r.Handle("/login", LoginHandler(store, gorm)).Methods("GET", "POST")
+	r.Handle("/register", RegisterHandler(store, gorm)).Methods("GET", "POST")
+	r.Handle("/logout", LogoutHandler(store, gorm)).Methods("GET")
+	r.Handle("/add_message", AddMessageHandler(store, gorm)).Methods("POST")
+	// r.Handle("/{username}", TestHandler(sql)).Methods("GET")
+	// r.Handle("/{username}/follow", TestHandler(sql)).Methods("GET")
+	// r.Handle("/{username}/unfollow", TestHandler(sql)).Methods("GET")
+	// r.Handle("/test", TestHandler(sql)).Methods("GET")
+	r.Handle("/user/{id}", GetUserByIdHandler(gorm)).Methods("GET")
 
 	http.ListenAndServe(":8080", r)
 }
