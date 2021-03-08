f559b8ba45b522f79f4e0958ee8e5dbfa01c4e47
Vlad Croitoru <0croitoruvlad@gmail.com>
2021-03-08 21:39:10 +0100

    hotfix schema

--- itu-minitwit-api/schema-pql.sql
+++ itu-minitwit-api/schema-pql.sql
@@ -1,6 +1,7 @@
+ceate sequence user_id_seq;
 drop table if exists users;
 create table users (
-  user_id serial primary key,
+  user_id primary key not null default nextval('user_id_seq'),
   username varchar(256) not null,
   email varchar(256) not null,
   pw_hash varchar(256) not null
@@ -12,9 +13,10 @@ create table followers (
   whom_id integer
 );
 
+create sequence message_id_seq;
 drop table if exists messages;
 create table messages (
-  message_id serial primary key,
+  message_id primary key not null default nextval('message_id_seq'),
   author_id integer not null,
   text varchar(256) not null,
   pub_date integer,
